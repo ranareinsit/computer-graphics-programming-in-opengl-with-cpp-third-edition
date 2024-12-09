2ad49aeef0da064e30c75e75f39d4c9f1946c03e
Andrew <113452664+ranareinsit@users.noreply.github.com>
2024-12-09 20:39:31 +0800

    chapter-06

---
+++ chapter-06/program-01/main.cpp
@@ -0,0 +1,144 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <SOIL2/soil2.h>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <cmath>
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp> 
+#include <glm/gtc/matrix_transform.hpp> 
+#include "Sphere.h"
+#include "Utils.h"
+using namespace std;
+
+#define numVAOs 1
+#define numVBOs 3
+
+float aspect;
+float cameraX, cameraY, cameraZ;
+float sphLocX, sphLocY, sphLocZ;
+glm::mat4 pMat, vMat, mMat, mvMat;
+GLuint earthTexture;
+GLuint mvLoc, projLoc;
+GLuint renderingProgram;
+GLuint vao[numVAOs];
+GLuint vbo[numVBOs];
+int width, height;
+
+Sphere mySphere = Sphere(32);
+
+void setupVertices(void) {
+	std::vector<int> ind = mySphere.getIndices();
+	std::vector<glm::vec3> vert = mySphere.getVertices();
+	std::vector<glm::vec2> tex = mySphere.getTexCoords();
+	std::vector<glm::vec3> norm = mySphere.getNormals();
+	std::vector<float> pvalues;
+	std::vector<float> tvalues;
+	std::vector<float> nvalues;
+
+	int numIndices = mySphere.getNumIndices();
+	for (int i = 0; i < numIndices; i++) {
+		pvalues.push_back((vert[ind[i]]).x);
+		pvalues.push_back((vert[ind[i]]).y);
+		pvalues.push_back((vert[ind[i]]).z);
+		tvalues.push_back((tex[ind[i]]).s);
+		tvalues.push_back((tex[ind[i]]).t);
+		nvalues.push_back((norm[ind[i]]).x);
+		nvalues.push_back((norm[ind[i]]).y);
+		nvalues.push_back((norm[ind[i]]).z);
+	}
+
+	glGenVertexArrays(1, vao);
+	glBindVertexArray(vao[0]);
+	glGenBuffers(numVBOs, vbo);
+
+	glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+	glBufferData(GL_ARRAY_BUFFER, pvalues.size() * 4, &pvalues[0], GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ARRAY_BUFFER, vbo[1]);
+	glBufferData(GL_ARRAY_BUFFER, tvalues.size() * 4, &tvalues[0], GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ARRAY_BUFFER, vbo[2]);
+	glBufferData(GL_ARRAY_BUFFER, nvalues.size() * 4, &nvalues[0], GL_STATIC_DRAW);
+}
+
+void init(GLFWwindow* window) {
+	renderingProgram = Utils::createShaderProgram("vertShader.glsl", "fragShader.glsl");
+	earthTexture = Utils::loadTexture("earth.jpg");
+
+	cameraX = 0.0f; cameraY = 0.0f; cameraZ = 3.0f;
+	sphLocX = 0.0f; sphLocY = 0.0f; sphLocZ = -1.0f;
+
+	glfwGetFramebufferSize(window, &width, &height);
+	aspect = (float)(width / height);
+	pMat = glm::perspective(1.0f, aspect, 0.1f, 1000.0f);
+
+	setupVertices();
+}
+
+void display(GLFWwindow* window, double currentTime) {
+	glClear(GL_DEPTH_BUFFER_BIT);
+	glClearColor(0.0, 0.0, 0.0, 1.0);
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glUseProgram(renderingProgram);
+
+	mvLoc = glGetUniformLocation(renderingProgram, "mv_matrix");
+	projLoc = glGetUniformLocation(renderingProgram, "proj_matrix");
+
+	vMat = glm::translate(glm::mat4(0.01f), glm::vec3(-cameraX, -cameraY, -cameraZ));
+	mMat = glm::rotate(glm::mat4(1.0f), (float)currentTime, glm::vec3(0.0f, -0.2f, -0.1f));
+
+	mvMat = vMat * mMat;
+
+	glUniformMatrix4fv(mvLoc, 1, GL_FALSE, glm::value_ptr(mvMat));
+	glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(pMat));
+
+	glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+	glEnableVertexAttribArray(0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, vbo[1]);
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 0, 0);
+	glEnableVertexAttribArray(1);
+
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, earthTexture);
+
+	glEnable(GL_CULL_FACE);
+	glFrontFace(GL_CCW);
+	glEnable(GL_DEPTH_TEST);
+	glDepthFunc(GL_LEQUAL);
+	glDrawArrays(GL_TRIANGLES, 0, mySphere.getNumIndices());
+}
+
+void window_size_callback(GLFWwindow* win, int newWidth, int newHeight) {
+	aspect = (float)(newWidth / newHeight);
+	glViewport(0, 0, newWidth, newHeight);
+	pMat = glm::perspective(1.0f, aspect, 0.1f, 1000.0f);
+}
+
+int main(void) {
+	if (!glfwInit()) { exit(EXIT_FAILURE); }
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	GLFWwindow* window = glfwCreateWindow(600, 600, "program 0601", NULL, NULL);
+	glfwMakeContextCurrent(window);
+	if (glewInit() != GLEW_OK) { exit(EXIT_FAILURE); }
+	glfwSwapInterval(1);
+
+	glfwSetWindowSizeCallback(window, window_size_callback);
+
+	init(window);
+
+	while (!glfwWindowShouldClose(window)) {
+		display(window, glfwGetTime());
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
+	exit(EXIT_SUCCESS);
+}
